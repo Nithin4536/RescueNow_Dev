7c11441b819c1978648a10063071a2c9db822617
Nithin4536 <reddynithin695@gmail.com>
2020-05-11 16:28:40 -0400

    Added App logo to splash screen. Added Firebase to the app

--- app/src/main/java/com/example/rescuenow_dev/SplashScreenActivity.java
+++ app/src/main/java/com/example/rescuenow_dev/SplashScreenActivity.java
@@ -2,13 +2,28 @@ package com.example.rescuenow_dev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent DashboardIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                DashboardIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(DashboardIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
