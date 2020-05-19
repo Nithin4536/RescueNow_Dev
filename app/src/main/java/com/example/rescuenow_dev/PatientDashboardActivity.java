12b6e23b1b3872737755620c8174092b6e76708d
pranay7v <pranay7v@gmail.com>
2020-05-19 18:46:08 -0400

    Toolbar menu click, signout for patient

--- app/src/main/java/com/example/rescuenow_dev/PatientDashboardActivity.java
+++ app/src/main/java/com/example/rescuenow_dev/PatientDashboardActivity.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class PatientDashboardActivity extends AppCompatActivity {
 
-    Button mSignout;
+
     private FirebaseAuth mAuth;
+    MaterialToolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,19 +28,35 @@ public class PatientDashboardActivity extends AppCompatActivity {
         //Firebase instance
         mAuth = FirebaseAuth.getInstance();
 
-        mSignout = findViewById(R.id.button_signout);
-        mSignout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_signout: {
                 mAuth.signOut();
                 Intent intent = new Intent(PatientDashboardActivity.this, LoginActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-
             }
-        });
-
-
-
+            case R.id.toolbar_profile: {
+                Toast.makeText(this, "profile clicked", Toast.LENGTH_SHORT).show();
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
