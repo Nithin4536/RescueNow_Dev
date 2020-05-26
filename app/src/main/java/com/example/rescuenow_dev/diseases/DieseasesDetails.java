f6e389bac3adf3a346939f21fea910949e49abf6
iamsawasim <s.abdulwasim96@gmail.com>
2020-05-25 23:46:29 -0400

    Disease Description details - wasim

---
+++ app/src/main/java/com/example/rescuenow_dev/diseases/DieseasesDetails.java
@@ -0,0 +1,16 @@
+package com.example.rescuenow_dev.diseases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rescuenow_dev.R;
+
+public class DieseasesDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dieseases_details);
+    }
+}
