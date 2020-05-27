bab54cd86c17207be6db3a5c52265c569bfb83ed
pranay7v <pranay7v@gmail.com>
2020-05-26 22:24:00 -0400

    UI elements init

--- app/src/main/java/com/example/rescuenow_dev/diseases/DieseasesDetails.java
+++ app/src/main/java/com/example/rescuenow_dev/diseases/DieseasesDetails.java
@@ -3,14 +3,30 @@ package com.example.rescuenow_dev.diseases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.rescuenow_dev.R;
 
 public class DieseasesDetails extends AppCompatActivity {
 
+
+    TextView diseaseTitle, diseaseDescription, diseaseSymptoms, diseasePrecautions, diseaseMedicines;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dieseases_details);
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        diseaseDescription = findViewById(R.id.dis_description);
+        diseaseTitle = findViewById(R.id.dis_name);
+        diseaseSymptoms = findViewById(R.id.dis_symptoms);
+        diseasePrecautions = findViewById(R.id.dis_precautions);
+        diseaseMedicines = findViewById(R.id.dis_medicines);
+
+
     }
 }
